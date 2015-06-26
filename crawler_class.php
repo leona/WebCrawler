a365cab0535e128196fcb7d79936ba16538fdde6
Leon Harvey <leonharvey@live.com>
2015-06-26 13:55:11 +0000

    update

--- crawler_class.php
+++ crawler_class.php
@@ -1,73 +1,124 @@
 <?php
-
 namespace leonhardly;
+require('multi.php');
+
+use jonathonhill\Multiprocess;
 
 class crawler {
-    
+
     public $site_store = array();
-    private $work_array;
-    private $dom;
-    private $work_url;
-    private $work_url_host;
+    private $work_array = array();
+    private $dom = array();
+    private $work_url = array();
+    private $work_url_host = array();
     private $count;
+    private $iteration_count;
     private $condition;
-    
+    private $sites_crawled = array();
+
+    public function __construct() {
+        $this->iteration_count = 0;
+        $this->count = 0;
+    }
+
     public function condition($condition = null) {
         $this->condition = $condition;
-        
+
         return $this;
     }
-    
-    public function start($url, $length) {
-        $this->load($url);
 
-        for($i = 0; $i < $length;$i++) {
-            if (empty($this->site_store[$i])) {
+    public function start($url, $length) {
+        $this->processThread($url);
+
+        do {
+            if (empty($this->site_store[$this->iteration_count])) {
                 echo '<br>No more URLs found<br>';
                 break;
             }
-  
-            $this->load($this->site_store[$i]);
-        }
+
+            if ($this->iteration_count > 20 && $this->iteration_count <= $this->count - 20) {
+
+                $cut = array_slice($this->site_store, $this->iteration_count - 1);
+
+                (new Multiprocess($cut, function($value) {
+                    $this->processThread($value);
+                }))->run(20);
+            } else {
+
+                $this->processThread($this->site_store[$this->iteration_count]);
+            }
+        } while($this->count < $length);
+
         $this->site_store = array_unique($this->site_store);
+
+        return $this;
     }
-    
-    private function loadURLs() {
-        $this->work_array = array();
-        
-        foreach($this->dom->getElementsByTagname('a') as $link) {
-            $this->validateURL($link->getAttribute('href'));
+
+    public function processThread($value) {
+        if (!in_array($value, $this->sites_crawled)) {
+            $this->sites_crawled[] = $value;
+            $this->load($value);
         }
-        
+    }
+
+    public function load($url) {
+        $this->iteration_count++;
+
+        try {
+            $this->dom[$url]  = new \DOMDocument();
+            $this->html[$url] = @file_get_contents($url);
+
+            if (empty($this->html[$url])) {
+                echo 'HTTP failure: ' . $url . "\r\n";
+                return false;
+            }
+
+            @$this->dom[$url]->loadHTML($this->html[$url]);
+        } catch(Exception $e) {}
+
+        $this->loadURLs($url);
+
+        return $this;
+    }
+
+    private function loadURLs($url) {
+        $origin_host = parse_url($url)['host'];
+        foreach($this->dom[$url]->getElementsByTagname('a') as $link) {
+            $this->validateURL($link->getAttribute('href'), $origin_host);
+        }
+
         return $this->work_array;
     }
-    
-    private function validateURL($url) {
+
+    public function afterLoop($callback) {
+        $new_store = array();
+
+        foreach($this->site_store as $key => $url) {
+            $result = $callback($key, $url);
+
+            if ($result !== false) $new_store[] = $result;
+        }
+
+        $this->site_store = array_filter($new_store);
+    }
+
+    private function validateURL($url, $origin_host) {
         if (!filter_var($url, FILTER_VALIDATE_URL) === false) {
             $href = parse_url($url);
 
-            if ($href['host'] !== $this->work_url_parsed['host']) {
+            if (!empty($href['host']) && $href['host'] !== $origin_host) {
                 $condition = $this->condition;
-                if ($condition == null || $condition($url))
-                    $this->site_store[] = $href['scheme'] . '://' . $href['host'];
+                if ($condition == null || $condition($url)) {
+                    $url = $href['scheme'] . '://' . $href['host'];
+                    if (!in_array($url, $this->site_store)) {
+                        $this->site_store[] = $url;
+                        $this->count++;
+                        echo $url . "\r\n";
+                    }
+                }
+
             }
         }
     }
-    private function load($url) {
-        $this->work_url = $url;
-        $this->work_url_parsed = parse_url($url);
-        $this->count++;
-        
-        try {
-            $this->dom = new \DOMDocument();
-            $this->html = @file_get_contents($this->work_url);
-            
-            if (!$this->html) return false;
-            
-            @$this->dom->loadHTML(file_get_contents($this->work_url));
-        } catch(Exception $e) {}
-        
-        $this->loadURLs();
-        return $this;
-    }
+
 }
--- example.php
+++ example.php
@@ -2,11 +2,12 @@
 require('crawler_class.php');
 
 use leonhardly\crawler;
-
 $crawler = new crawler;
 
-$crawler->condition(function($url) {
-    return true;
-})->start('https://www.google.ie/search?q=random', 5);
+$crawler->start($argv[1], 200);
 
-print_r($crawler->site_store);
+
+
+
+
+
